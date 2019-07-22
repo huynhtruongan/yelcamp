d5870517cc3ec2f9198439e4c78f08a35ea189a8
huynhtruongan <huhuhaha.not@gmail.com>
2019-07-22 21:52:34 +0700

    add v3, empty file  comment.js

---
+++ v3/models/campground.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+let campgroundSchema = new mongoose.Schema({
+    name : String,
+    image : String,
+    description : String
+})
+module.exports = mongoose.model ('Campground' , campgroundSchema);
